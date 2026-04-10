55df69e27aba77c5e051e72a5f53e6b562da9cf6
Enes Yılmaz <enes213132@ogr.duzce.edu.tr>
2026-04-11 02:45:30 +0300

    Add HomeActivity and initial UI layout

--- app/src/main/java/com/example/dijitalraf/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/dijitalraf/ui/home/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.dijitalraf.ui.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,15 +13,10 @@ import com.example.dijitalraf.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
