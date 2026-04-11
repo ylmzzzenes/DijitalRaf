a1850254862979ca677a17824d1a33803f69e6d5
Enes Yılmaz <enes213132@ogr.duzce.edu.tr>
2026-04-12 01:18:20 +0300

    Implement logout functionality with FirebaseAuth and clean activity flow

--- app/src/main/java/com/example/dijitalraf/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/dijitalraf/ui/home/HomeActivity.java
@@ -2,21 +2,50 @@ package com.example.dijitalraf.ui.home;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dijitalraf.R;
+import com.example.dijitalraf.ui.auth.LoginActivity;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 import com.example.dijitalraf.R;
 
 public class HomeActivity extends AppCompatActivity {
+private FirebaseAuth mAuth;
+private Button btnLogout;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initComponents();
+        registerEventHandlers();
+    }
+    private void initComponents()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.btnLogout);
+    }
+
+    private void registerEventHandlers()
+    {
+        btnLogout.setOnClickListener(v -> logoutUser());
+    }
+
+    private void logoutUser()
+    {
+        mAuth.signOut();
+
+        Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
