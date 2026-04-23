217a2475bb62ab6031143f0072346593d77e56ef
zeyneppcakir <zeynepcakir.3966@gmail.com>
2026-04-23 13:15:14 +0300

    added kitaplarim screen

---
+++ app/src/main/java/com/example/dijitalraf/ui/home/KitaplarimActivity.java
@@ -0,0 +1,15 @@
+package com.example.dijitalraf.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dijitalraf.R;
+
+public class KitaplarimActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitaplarim);
+    }
+}
